b6e0fb3eba5719b7d7da98da7e0f273e361505d6
Bruno Felix <brunofelix.dev@gmail.com>
2026-06-23 23:00:42 -0300

    ♻️ Making movie routes serializable

--- shared/src/commonMain/kotlin/dev/brunofelix/presentation/navigation/MovieRoute.kt
+++ shared/src/commonMain/kotlin/dev/brunofelix/presentation/navigation/MovieRoute.kt
@@ -1,6 +1,11 @@
 package dev.brunofelix.presentation.navigation
 
+import kotlinx.serialization.Serializable
+
 sealed class MovieRoute {
+    @Serializable
     data object ListScreen : MovieRoute()
+    
+    @Serializable
     data class DetailScreen(val id: Int) : MovieRoute()
 }
