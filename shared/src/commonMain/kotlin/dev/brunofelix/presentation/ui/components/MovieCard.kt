01eeca8ce2ea5e2682f98ada90d79fa4a98b8270
Bruno Felix <brunofelix.dev@gmail.com>
2026-06-24 15:18:52 -0300

    💄 Implementing MovieCard component and updating App preview settings

---
+++ shared/src/commonMain/kotlin/dev/brunofelix/presentation/ui/components/MovieCard.kt
@@ -0,0 +1,41 @@
+package dev.brunofelix.presentation.ui.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import movies_kmp.shared.generated.resources.Res
+import movies_kmp.shared.generated.resources.poster_path
+import org.jetbrains.compose.resources.painterResource
+
+@Composable
+@Preview
+fun MovieCard(
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier
+    ) {
+        Card(
+            modifier = Modifier
+                .width(150.dp)
+                .height(220.dp),
+            shape = RoundedCornerShape(12.dp)
+        ) {
+            Image(
+                painter = painterResource(Res.drawable.poster_path),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxSize()
+            )
+        }
+    }
+}
