626c668d541ae2a9f24788471ab0dc6ef6b85a3f
Bruno Felix <brunofelix.dev@gmail.com>
2026-06-26 23:52:31 -0300

    💄 Implementing MovieListScreen UI

---
+++ shared/src/commonMain/kotlin/dev/brunofelix/presentation/ui/MovieListScreen.kt
@@ -0,0 +1,39 @@
+package dev.brunofelix.presentation.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import dev.brunofelix.presentation.ui.components.MovieCard
+
+@Preview
+@Composable
+fun MovieListScreen() {
+    Scaffold { paddingValues ->
+        LazyColumn(
+            modifier = Modifier.padding(paddingValues),
+            contentPadding = PaddingValues(16.dp),
+        ) {
+            item {
+                Column {
+                    LazyRow(
+                        modifier = Modifier.padding(top = 8.dp),
+                        contentPadding = PaddingValues(horizontal = 8.dp),
+                        horizontalArrangement = Arrangement.spacedBy(8.dp)
+                    ) {
+                        items(5) {
+                            MovieCard()
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
