e5f4c8eabb02b541cb1420ab0451da41d5433c29
Bruno Felix <brunofelix.dev@gmail.com>
2026-06-28 00:08:08 -0300

    💄 Implementing MovieSection component

---
+++ shared/src/commonMain/kotlin/dev/brunofelix/presentation/ui/components/MovieSection.kt
@@ -0,0 +1,38 @@
+package dev.brunofelix.presentation.ui.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import dev.brunofelix.presentation.state.MovieUiState
+
+@Composable
+fun MovieSection(
+    title: String,
+    movies: List<MovieUiState>,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier
+    ) {
+        Text(
+            text = title,
+            style = MaterialTheme.typography.titleLarge,
+            color = MaterialTheme.colorScheme.primary
+        )
+        LazyRow(
+            modifier = Modifier.padding(top = 16.dp, bottom = 16.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            items(movies) { movie ->
+                MovieCard(movie)
+            }
+        }
+    }
+}
