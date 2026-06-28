6ebb2e8fa06e447c28c53e8117ad6ff44a7d235f
Bruno Felix <brunofelix.dev@gmail.com>
2026-06-28 00:08:51 -0300

    💄 Implementing MovieSection and expanding MovieListScreen with more categories

--- shared/src/commonMain/kotlin/dev/brunofelix/presentation/ui/MovieListScreen.kt
+++ shared/src/commonMain/kotlin/dev/brunofelix/presentation/ui/MovieListScreen.kt
@@ -1,21 +1,19 @@
 package dev.brunofelix.presentation.ui
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import dev.brunofelix.presentation.ui.components.MovieCard
+import dev.brunofelix.presentation.state.fakeMovie
+import dev.brunofelix.presentation.ui.components.MovieSection
 import movies_kmp.shared.generated.resources.Res
 import movies_kmp.shared.generated.resources.populars
+import movies_kmp.shared.generated.resources.top_rated
+import movies_kmp.shared.generated.resources.upcoming
 import org.jetbrains.compose.resources.stringResource
 
 @Preview
@@ -31,21 +29,28 @@ fun MovieListScreen(
             contentPadding = PaddingValues(16.dp),
         ) {
             item {
-                Column {
-                    Text(
-                        text = stringResource(Res.string.populars),
-                        style = MaterialTheme.typography.titleLarge,
-                        color = MaterialTheme.colorScheme.primary
-                    )
-                    LazyRow(
-                        modifier = Modifier.padding(top = 16.dp),
-                        horizontalArrangement = Arrangement.spacedBy(8.dp)
-                    ) {
-                        items(5) {
-                            MovieCard()
-                        }
+                MovieSection(
+                    title = stringResource(Res.string.populars),
+                    movies = List(10) {
+                        fakeMovie
                     }
-                }
+                )
+            }
+            item {
+                MovieSection(
+                    title = stringResource(Res.string.upcoming),
+                    movies = List(10) {
+                        fakeMovie
+                    }
+                )
+            }
+            item {
+                MovieSection(
+                    title = stringResource(Res.string.top_rated),
+                    movies = List(10) {
+                        fakeMovie
+                    }
+                )
             }
         }
     }
