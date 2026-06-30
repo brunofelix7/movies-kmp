a7d7edaa45fabb2dee906b3ad1718a58994c7494
Bruno Felix <brunofelix.dev@gmail.com>
2026-06-29 23:57:30 -0300

    🌐 Configuring Ktor client logging

--- shared/src/commonMain/kotlin/dev/brunofelix/data/api/KtorClient.kt
+++ shared/src/commonMain/kotlin/dev/brunofelix/data/api/KtorClient.kt
@@ -1,6 +1,13 @@
 package dev.brunofelix.data.api
 
 import io.ktor.client.HttpClient
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.plugins.logging.SIMPLE
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
 
 object KtorClient {
 
@@ -13,6 +20,10 @@ object KtorClient {
                     ignoreUnknownKeys = true
                 }
             )
+        }
+        install(Logging) {
+            logger = Logger.SIMPLE
+            level = LogLevel.ALL
         }
     }
 }
