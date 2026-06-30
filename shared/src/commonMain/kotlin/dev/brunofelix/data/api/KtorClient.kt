c1209bf99df9727153ddcdb063371714a90a78c5
Bruno Felix <brunofelix.dev@gmail.com>
2026-06-30 00:01:36 -0300

    🔒️ Sanitizing Authorization header in Ktor logging

--- shared/src/commonMain/kotlin/dev/brunofelix/data/api/KtorClient.kt
+++ shared/src/commonMain/kotlin/dev/brunofelix/data/api/KtorClient.kt
@@ -9,6 +9,7 @@ import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.plugins.logging.SIMPLE
+import io.ktor.http.HttpHeaders
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 
@@ -27,6 +28,7 @@ object KtorClient {
         install(Logging) {
             logger = Logger.SIMPLE
             level = LogLevel.ALL
+            sanitizeHeader { header -> header == HttpHeaders.Authorization }
         }
         install(Auth) {
             bearer {
