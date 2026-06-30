79dff69e0bedce377fcee6b558576228bb0e0e71
Bruno Felix <brunofelix.dev@gmail.com>
2026-06-29 23:55:49 -0300

    🌐 Implementing KtorClient with JSON configuration

---
+++ shared/src/commonMain/kotlin/dev/brunofelix/data/api/KtorClient.kt
@@ -0,0 +1,18 @@
+package dev.brunofelix.data.api
+
+import io.ktor.client.HttpClient
+
+object KtorClient {
+
+    private val client = HttpClient {
+        install(ContentNegotiation) {
+            json(
+                Json {
+                    prettyPrint = true
+                    isLenient = true
+                    ignoreUnknownKeys = true
+                }
+            )
+        }
+    }
+}
