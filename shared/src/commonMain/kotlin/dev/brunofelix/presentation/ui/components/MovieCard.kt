d535f9d99c953e8855106c2e79d2b5e04a47fcdf
Bruno Felix <brunofelix.dev@gmail.com>
2026-07-02 16:52:17 -0300

    💄 Adding Preview to MovieCard

--- shared/src/commonMain/kotlin/dev/brunofelix/presentation/ui/components/MovieCard.kt
+++ shared/src/commonMain/kotlin/dev/brunofelix/presentation/ui/components/MovieCard.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import dev.brunofelix.presentation.state.MovieUiState
 import movies_kmp.shared.generated.resources.Res
@@ -38,4 +39,12 @@ fun MovieCard(
             )
         }
     }
+}
+
+@Preview
+@Composable
+private fun Preview() {
+    MovieCard(
+        movie = MovieUiState()
+    )
 }
