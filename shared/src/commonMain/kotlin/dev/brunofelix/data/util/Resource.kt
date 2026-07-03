aa6f887cb3066ec7f6524db091316f918dad7ea8
Bruno Felix <brunofelix.dev@gmail.com>
2026-07-02 23:13:17 -0300

    🔨 Defining CategoryType, Resource, and UiState utility classes

---
+++ shared/src/commonMain/kotlin/dev/brunofelix/data/util/Resource.kt
@@ -0,0 +1,6 @@
+package dev.brunofelix.data.util
+
+sealed class Resource<out T> {
+    data class Success<T>(val data: T) : Resource<T>()
+    data class Error(val throwable: Throwable) : Resource<Nothing>()
+}
---
+++ shared/src/commonMain/kotlin/dev/brunofelix/domain/util/CategoryType.kt
@@ -0,0 +1,9 @@
+package dev.brunofelix.domain.util
+
+enum class CategoryType(
+    val value: String
+) {
+    POPULAR("popular"),
+    TOP_RATED("top_rated"),
+    UPCOMING("upcoming")
+}
