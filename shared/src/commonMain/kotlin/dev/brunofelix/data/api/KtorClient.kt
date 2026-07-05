4b1e7536e7ad1a06335b2be3ca43e3d7fea5caeb
Bruno Felix <brunofelix.dev@gmail.com>
2026-07-04 22:50:12 -0300

    🌐 Configuring HttpResponseValidator to map exceptions to NetworkException

--- shared/src/commonMain/kotlin/dev/brunofelix/data/api/KtorClient.kt
+++ shared/src/commonMain/kotlin/dev/brunofelix/data/api/KtorClient.kt
@@ -1,7 +1,11 @@
 package dev.brunofelix.data.api
 
 import dev.brunofelix.BuildKonfig
+import dev.brunofelix.data.util.NetworkException
 import io.ktor.client.HttpClient
+import io.ktor.client.plugins.ClientRequestException
+import io.ktor.client.plugins.HttpResponseValidator
+import io.ktor.client.plugins.ServerResponseException
 import io.ktor.client.plugins.auth.Auth
 import io.ktor.client.plugins.auth.providers.BearerTokens
 import io.ktor.client.plugins.auth.providers.bearer
@@ -13,11 +17,30 @@ import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.plugins.logging.SIMPLE
 import io.ktor.http.HttpHeaders
 import io.ktor.serialization.kotlinx.json.json
+import kotlinx.io.IOException
 import kotlinx.serialization.json.Json
 
 object KtorClient {
 
     val client = HttpClient {
+        expectSuccess = true
+
+        HttpResponseValidator {
+            handleResponseExceptionWithRequest { exception, request ->
+                when (exception) {
+                    is ClientRequestException -> {
+                        when (exception.response.status.value) {
+                            401 -> throw NetworkException.Unauthorized()
+                            404 -> throw NetworkException.NotFound()
+                            else -> throw NetworkException.Unknown()
+                        }
+                    }
+                    is ServerResponseException -> throw NetworkException.ServerError()
+                    is IOException -> throw NetworkException.NoInternet()
+                    else -> throw NetworkException.Unknown()
+                }
+            }
+        }
         defaultRequest {
             url(BuildKonfig.BASE_URL)
         }
