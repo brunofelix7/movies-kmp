00d180b2012b1e59b122a14701371abbd9c2d938
Bruno Felix <brunofelix.dev@gmail.com>
2026-07-04 22:53:03 -0300

    🏗️ Refactoring MovieViewModel to use GetMoviesUseCase and implementing UiEvent

--- shared/src/commonMain/kotlin/dev/brunofelix/presentation/viewmodel/MovieViewModel.kt
+++ shared/src/commonMain/kotlin/dev/brunofelix/presentation/viewmodel/MovieViewModel.kt
@@ -3,32 +3,47 @@ package dev.brunofelix.presentation.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dev.brunofelix.domain.model.MovieSection
-import dev.brunofelix.domain.repository.MovieRepository
+import dev.brunofelix.domain.use_case.GetMoviesUseCase
+import dev.brunofelix.domain.util.Resource
+import dev.brunofelix.presentation.util.UiEvent
 import dev.brunofelix.presentation.util.UiState
+import dev.brunofelix.presentation.util.toUiState
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 class MovieViewModel(
-    private val repository: MovieRepository
+    private val getMoviesUseCase: GetMoviesUseCase
 ) : ViewModel() {
 
     private val _state = MutableStateFlow<UiState<List<MovieSection>>>(UiState.Loading)
-    val state = _state.asStateFlow()
+    val state = _state
+        .onStart { getMoviesSection() }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000L),
+            initialValue = UiState.Loading
+        )
 
-    init {
-        getMoviesSection()
-    }
+    private val _event = Channel<UiEvent>()
+    val event = _event.receiveAsFlow()
 
     private fun getMoviesSection() {
         viewModelScope.launch {
-            try {
-                val movies = repository.getMovies()
-                _state.update { UiState.Success(movies) }
-            } catch (e: Exception) {
-                _state.update { UiState.Error(e.message ?: "Unknown error") }
+            _state.update { UiState.Loading }
+            val uiState = when (val result = getMoviesUseCase()) {
+                is Resource.Success -> result.toUiState()
+                is Resource.Error -> {
+                    _event.send(UiEvent.ShowErrorMessage(result.exception))
+                    result.toUiState()
+                }
             }
+            _state.update { uiState }
         }
     }
 }
