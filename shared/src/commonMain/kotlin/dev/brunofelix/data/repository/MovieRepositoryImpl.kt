a9d95b56b62672f2645449cdc99b27a869dc9fe2
Bruno Felix <brunofelix.dev@gmail.com>
2026-07-04 22:51:58 -0300

    🔌 Updating MovieRepository and MovieRepositoryImpl to return Resource and handling results with runCatching

--- shared/src/commonMain/kotlin/dev/brunofelix/data/repository/MovieRepositoryImpl.kt
+++ shared/src/commonMain/kotlin/dev/brunofelix/data/repository/MovieRepositoryImpl.kt
@@ -2,36 +2,49 @@ package dev.brunofelix.data.repository
 
 import dev.brunofelix.data.api.KtorService
 import dev.brunofelix.data.api.mapper.toDomain
+import dev.brunofelix.domain.util.Resource
+import dev.brunofelix.domain.util.toResource
 import dev.brunofelix.domain.model.MovieSection
 import dev.brunofelix.domain.model.enums.CategoryType
 import dev.brunofelix.domain.repository.MovieRepository
 import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.IO
 import kotlinx.coroutines.async
 import kotlinx.coroutines.withContext
 
 class MovieRepositoryImpl(
     private val api: KtorService,
-    private val dispatcher: CoroutineDispatcher = Dispatchers.IO
+    private val dispatcher: CoroutineDispatcher
 ) : MovieRepository {
 
-    override suspend fun getMovies(): List<MovieSection> {
+    override suspend fun getMovies(): Resource<List<MovieSection>> {
         return withContext(dispatcher) {
-            val popularMovies = async {
-                api.getMovies(CategoryType.POPULAR.value).results?.map { it.toDomain() }
-            }.await()
-            val topRatedMovies = async {
-                api.getMovies(CategoryType.TOP_RATED.value).results?.map { it.toDomain() }
-            }.await()
-            val upcomingMovies = async {
-                api.getMovies(CategoryType.UPCOMING.value).results?.map { it.toDomain() }
-            }.await()
-            listOf(
-                MovieSection(CategoryType.POPULAR, popularMovies ?: emptyList()),
-                MovieSection(CategoryType.TOP_RATED, topRatedMovies ?: emptyList()),
-                MovieSection(CategoryType.UPCOMING, upcomingMovies ?: emptyList())
-            )
+            runCatching {
+                val popularDeferred = async {
+                    api.getMovies(CategoryType.POPULAR.value).results?.map {
+                        it.toDomain()
+                    } ?: emptyList()
+                }
+                val topRatedDeferred = async {
+                    api.getMovies(CategoryType.TOP_RATED.value).results?.map {
+                        it.toDomain()
+                    } ?: emptyList()
+                }
+                val upcomingDeferred = async {
+                    api.getMovies(CategoryType.UPCOMING.value).results?.map {
+                        it.toDomain()
+                    } ?: emptyList()
+                }
+
+                val popularMovies = popularDeferred.await()
+                val topRatedMovies = topRatedDeferred.await()
+                val upcomingMovies = upcomingDeferred.await()
+
+                listOf(
+                    MovieSection(CategoryType.POPULAR, popularMovies),
+                    MovieSection(CategoryType.TOP_RATED, topRatedMovies),
+                    MovieSection(CategoryType.UPCOMING, upcomingMovies)
+                )
+            }.toResource()
         }
     }
 }
--- shared/src/commonMain/kotlin/dev/brunofelix/domain/repository/MovieRepository.kt
+++ shared/src/commonMain/kotlin/dev/brunofelix/domain/repository/MovieRepository.kt
@@ -1,7 +1,8 @@
 package dev.brunofelix.domain.repository
 
+import dev.brunofelix.domain.util.Resource
 import dev.brunofelix.domain.model.MovieSection
 
 interface MovieRepository {
-    suspend fun getMovies(): List<MovieSection>
+    suspend fun getMovies(): Resource<List<MovieSection>>
 }
