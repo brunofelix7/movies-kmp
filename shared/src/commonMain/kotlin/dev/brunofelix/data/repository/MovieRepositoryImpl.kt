cf897e14b50a250f57604fb2cb1f27d48e99d6c1
Bruno Felix <brunofelix.dev@gmail.com>
2026-07-04 23:05:48 -0300

    ♻️ Refactoring MovieRepositoryImpl to use MovieRemoteDataSource

--- shared/src/commonMain/kotlin/dev/brunofelix/data/repository/MovieRepositoryImpl.kt
+++ shared/src/commonMain/kotlin/dev/brunofelix/data/repository/MovieRepositoryImpl.kt
@@ -1,43 +1,30 @@
 package dev.brunofelix.data.repository
 
-import dev.brunofelix.data.api.KtorService
-import dev.brunofelix.data.api.mapper.toDomain
-import dev.brunofelix.domain.util.Resource
-import dev.brunofelix.domain.util.toResource
+import dev.brunofelix.data.source.MovieRemoteDataSource
 import dev.brunofelix.domain.model.MovieSection
 import dev.brunofelix.domain.model.enums.CategoryType
 import dev.brunofelix.domain.repository.MovieRepository
+import dev.brunofelix.domain.util.Resource
+import dev.brunofelix.domain.util.toResource
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.async
 import kotlinx.coroutines.withContext
 
 class MovieRepositoryImpl(
-    private val api: KtorService,
+    private val remoteDataSource: MovieRemoteDataSource,
     private val dispatcher: CoroutineDispatcher
 ) : MovieRepository {
 
     override suspend fun getMovies(): Resource<List<MovieSection>> {
         return withContext(dispatcher) {
             runCatching {
-                val popularDeferred = async {
-                    api.getMovies(CategoryType.POPULAR.value).results?.map {
-                        it.toDomain()
-                    } ?: emptyList()
-                }
-                val topRatedDeferred = async {
-                    api.getMovies(CategoryType.TOP_RATED.value).results?.map {
-                        it.toDomain()
-                    } ?: emptyList()
-                }
-                val upcomingDeferred = async {
-                    api.getMovies(CategoryType.UPCOMING.value).results?.map {
-                        it.toDomain()
-                    } ?: emptyList()
-                }
+                val popularDeferred = async { remoteDataSource.getPopular() }
+                val topRatedDeferred = async { remoteDataSource.getTopRated() }
+                val upcomingDeferred = async { remoteDataSource.getUpcoming() }
 
-                val popularMovies = popularDeferred.await()
-                val topRatedMovies = topRatedDeferred.await()
-                val upcomingMovies = upcomingDeferred.await()
+                val popularMovies = popularDeferred.await().getOrThrow()
+                val topRatedMovies = topRatedDeferred.await().getOrThrow()
+                val upcomingMovies = upcomingDeferred.await().getOrThrow()
 
                 listOf(
                     MovieSection(CategoryType.POPULAR, popularMovies),
