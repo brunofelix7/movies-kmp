14956bc76d81ef3f6538aa4449c64d5813b422b3
Bruno Felix <brunofelix.dev@gmail.com>
2026-07-06 10:18:24 -0300

    🔥 Removing MovieSectionMapper and MovieSectionUiState

--- shared/src/commonMain/kotlin/dev/brunofelix/presentation/state/MovieSectionUiState.kt
+++
@@ -1,8 +0,0 @@
-package dev.brunofelix.presentation.state
-
-import dev.brunofelix.domain.model.enums.CategoryType
-
-data class MovieSectionUiState(
-    val sectionType: CategoryType = CategoryType.POPULAR,
-    val movies: List<MovieUiState> = emptyList()
-)
