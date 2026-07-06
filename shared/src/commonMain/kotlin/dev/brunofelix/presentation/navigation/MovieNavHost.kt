7256d4bf5ce70397883069bb82b35cf1322ec5b9
Bruno Felix <brunofelix.dev@gmail.com>
2026-07-05 23:33:59 -0300

    💄 Refactoring MovieNavHost to use movieGraph extension

--- shared/src/commonMain/kotlin/dev/brunofelix/presentation/navigation/MovieNavHost.kt
+++ shared/src/commonMain/kotlin/dev/brunofelix/presentation/navigation/MovieNavHost.kt
@@ -3,8 +3,6 @@ package dev.brunofelix.presentation.navigation
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import dev.brunofelix.presentation.ui.MovieListScreen
 
 @Composable
 fun MovieNavHost(
@@ -14,11 +12,13 @@ fun MovieNavHost(
         navController = navController,
         startDestination = MovieDestination.List
     ) {
-        composable<MovieDestination.List> {
-            MovieListScreen()
-        }
-        composable<MovieDestination.Detail> {
-            // TODO: Create detail screen
-        }
+        movieGraph(
+            onNavigateToDetail = { id ->
+                //navController.navigate(MovieDestination.Detail(id))
+            },
+            onBack = {
+                navController.popBackStack()
+            }
+        )
     }
 }
