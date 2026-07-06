877b05de179b9dbe1651e7716bbcffa0a1b4f1ad
Bruno Felix <brunofelix.dev@gmail.com>
2026-07-06 11:07:20 -0300

    🔥 Removing MovieMapper and MovieUiState

--- shared/src/commonMain/kotlin/dev/brunofelix/presentation/state/MovieUiState.kt
+++
@@ -1,8 +0,0 @@
-package dev.brunofelix.presentation.state
-
-data class MovieUiState(
-    val id: Int = 0,
-    val title: String = "",
-    val overview: String = "",
-    val posterPath: String = ""
-)
