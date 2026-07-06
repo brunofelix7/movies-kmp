c23f3391cc703915db7525ce3dfba854c3148301
Bruno Felix <brunofelix.dev@gmail.com>
2026-07-05 23:34:39 -0300

    🔥 Removing 'fakeMovie' from MovieUiState

--- shared/src/commonMain/kotlin/dev/brunofelix/presentation/state/MovieUiState.kt
+++ shared/src/commonMain/kotlin/dev/brunofelix/presentation/state/MovieUiState.kt
@@ -6,11 +6,3 @@ data class MovieUiState(
     val overview: String = "",
     val posterPath: String = ""
 )
-
-// Fake objects
-val fakeMovie = MovieUiState(
-    id = 1,
-    title = "Movie 1",
-    overview = "Overview 1",
-    posterPath = "https://example.com/poster1.jpg"
-)
