b885985c8d9b8bf1b677a1c2edd7edc2ed21b705
Bruno Felix <brunofelix.dev@gmail.com>
2026-07-08 00:25:20 -0300

    ♻️ Refactoring navigation to use sharedKoinViewModel for MovieViewModel

--- shared/src/commonMain/kotlin/dev/brunofelix/presentation/navigation/MovieNavGraph.kt
+++ shared/src/commonMain/kotlin/dev/brunofelix/presentation/navigation/MovieNavGraph.kt
@@ -4,14 +4,19 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import dev.brunofelix.presentation.ui.MovieListRoute
+import dev.brunofelix.presentation.util.sharedKoinViewModel
 import dev.brunofelix.presentation.viewmodel.MovieViewModel
 
 fun NavGraphBuilder.movieGraph(
-    navController: NavController,
-    viewModel: MovieViewModel
+    navController: NavController
 ) {
     composable<MovieDestination.List> {
-        MovieListRoute(navController, viewModel)
+        val viewModel = it.sharedKoinViewModel<MovieViewModel>(navController)
+
+        MovieListRoute(
+            navController = navController,
+            viewModel = viewModel
+        )
     }
     composable<MovieDestination.Detail> {
         // val movieId = it.toRoute<MovieDestination.Details>().id
--- shared/src/commonMain/kotlin/dev/brunofelix/presentation/navigation/MovieNavHost.kt
+++ shared/src/commonMain/kotlin/dev/brunofelix/presentation/navigation/MovieNavHost.kt
@@ -3,22 +3,15 @@ package dev.brunofelix.presentation.navigation
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
-import dev.brunofelix.presentation.viewmodel.MovieViewModel
-import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
 fun MovieNavHost(
     navController: NavHostController
 ) {
-    val viewModel = koinViewModel<MovieViewModel>()
-
     NavHost(
         navController = navController,
         startDestination = MovieDestination.List
     ) {
-        movieGraph(
-            navController = navController,
-            viewModel = viewModel
-        )
+        movieGraph(navController = navController)
     }
 }
