560aa8ce539b7b106dae974d8506ec643ed112c0
Bruno Felix <brunofelix.dev@gmail.com>
2026-07-07 23:31:25 -0300

    💉 Implementing Koin modules for dependency injection

---
+++ shared/src/commonMain/kotlin/dev/brunofelix/di/KoinModules.kt
@@ -0,0 +1,39 @@
+package dev.brunofelix.di
+
+import dev.brunofelix.data.api.KtorClient
+import dev.brunofelix.data.api.KtorClientImpl
+import dev.brunofelix.data.api.KtorService
+import dev.brunofelix.data.api.KtorServiceImpl
+import dev.brunofelix.data.repository.MovieRepositoryImpl
+import dev.brunofelix.data.source.MovieRemoteDataSource
+import dev.brunofelix.data.source.MovieRemoteDataSourceImpl
+import dev.brunofelix.domain.repository.MovieRepository
+import dev.brunofelix.domain.use_case.GetMoviesUseCase
+import dev.brunofelix.domain.use_case.GetMoviesUseCaseImpl
+import dev.brunofelix.presentation.viewmodel.MovieViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
+import org.koin.core.module.dsl.viewModel
+import org.koin.dsl.module
+
+val dispatcherModule = module {
+    single { Dispatchers.IO }
+}
+
+val networkModule = module {
+    single<KtorClient> { KtorClientImpl() }
+    single<KtorService> { KtorServiceImpl(get()) }
+}
+
+val dataModule = module {
+    single<MovieRemoteDataSource> { MovieRemoteDataSourceImpl(get()) }
+    single<MovieRepository> { MovieRepositoryImpl(get(), get()) }
+}
+
+val useCaseModule = module {
+    factory<GetMoviesUseCase> { GetMoviesUseCaseImpl(get()) }
+}
+
+val viewModelModule = module {
+    viewModel { MovieViewModel(get()) }
+}
