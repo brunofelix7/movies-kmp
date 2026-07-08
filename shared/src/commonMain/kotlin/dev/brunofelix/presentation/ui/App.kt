3f972266e927205dd76cfeb92e09a2ff1fbfc745
Bruno Felix <brunofelix.dev@gmail.com>
2026-07-07 23:33:23 -0300

    💉 Initializing Koin and loading dependency modules in App composable

--- shared/src/commonMain/kotlin/dev/brunofelix/presentation/ui/App.kt
+++ shared/src/commonMain/kotlin/dev/brunofelix/presentation/ui/App.kt
@@ -3,15 +3,32 @@ package dev.brunofelix.presentation.ui
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.navigation.compose.rememberNavController
+import dev.brunofelix.di.dataModule
+import dev.brunofelix.di.dispatcherModule
+import dev.brunofelix.di.networkModule
+import dev.brunofelix.di.useCaseModule
+import dev.brunofelix.di.viewModelModule
 import dev.brunofelix.presentation.navigation.MovieNavHost
 import dev.brunofelix.presentation.ui.resources.AppTheme
+import org.koin.compose.KoinApplication
+import org.koin.dsl.koinConfiguration
 
 @Composable
 @PreviewLightDark
 fun App() {
-    AppTheme {
-        MovieNavHost(
-            navController = rememberNavController()
+    KoinApplication(configuration = koinConfiguration {
+        modules(
+            dispatcherModule,
+            networkModule,
+            dataModule,
+            useCaseModule,
+            viewModelModule
         )
+    }) {
+        AppTheme {
+            MovieNavHost(
+                navController = rememberNavController()
+            )
+        }
     }
 }
