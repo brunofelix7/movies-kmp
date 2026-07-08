e54abe0635567c7648daa1d7ba81e4febd85384a
Bruno Felix <brunofelix.dev@gmail.com>
2026-07-07 23:42:22 -0300

    🌐 Refactoring KtorService to interface and implementing KtorServiceImpl

--- shared/src/commonMain/kotlin/dev/brunofelix/data/api/KtorService.kt
+++ shared/src/commonMain/kotlin/dev/brunofelix/data/api/KtorService.kt
@@ -1,7 +1,21 @@
 package dev.brunofelix.data.api
 
 import dev.brunofelix.data.api.dto.MovieListDto
+import io.ktor.client.call.body
+import io.ktor.client.request.get
+import io.ktor.client.request.parameter
 
 interface KtorService {
     suspend fun getMovies(category: String): MovieListDto
+}
+
+class KtorServiceImpl(
+    private val client: KtorClient
+) : KtorService {
+
+    override suspend fun getMovies(category: String): MovieListDto {
+        return client.httpClient.get("movie/$category") {
+            parameter("language", "en-US")
+        }.body()
+    }
 }
