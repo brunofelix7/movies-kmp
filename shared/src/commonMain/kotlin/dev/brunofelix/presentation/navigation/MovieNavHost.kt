0e699cf5d76f13d5a4f6117d9162bacba33c89a0
Bruno Felix <brunofelix.dev@gmail.com>
2026-07-07 23:42:59 -0300

    💉 Injecting MovieViewModel in MovieNavHost

--- shared/src/commonMain/kotlin/dev/brunofelix/presentation/navigation/MovieNavHost.kt
+++ shared/src/commonMain/kotlin/dev/brunofelix/presentation/navigation/MovieNavHost.kt
@@ -1,32 +1,17 @@
 package dev.brunofelix.presentation.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
-import dev.brunofelix.data.api.KtorClient
-import dev.brunofelix.data.api.KtorServiceImpl
-import dev.brunofelix.data.repository.MovieRepositoryImpl
-import dev.brunofelix.data.source.MovieRemoteDataSourceImpl
-import dev.brunofelix.domain.use_case.GetMoviesUseCaseImpl
 import dev.brunofelix.presentation.viewmodel.MovieViewModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.IO
+import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
 fun MovieNavHost(
     navController: NavHostController
 ) {
-    // Instanciação manual temporária encapsulada com remember
-    // para não recriar a infraestrutura em recomposições comuns
-    val client = KtorClient.client
-    val service = KtorServiceImpl(client)
-    val dataSource = MovieRemoteDataSourceImpl(service)
-    val repository = MovieRepositoryImpl(dataSource, Dispatchers.IO)
-    val useCase = GetMoviesUseCaseImpl(repository)
-    val viewModel: MovieViewModel = viewModel {
-        MovieViewModel(useCase)
-    }
+    val viewModel = koinViewModel<MovieViewModel>()
+
     NavHost(
         navController = navController,
         startDestination = MovieDestination.List
