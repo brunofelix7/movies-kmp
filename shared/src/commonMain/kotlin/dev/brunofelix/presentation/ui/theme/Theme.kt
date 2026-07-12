1ccf5a6afbfe9dc95ec899cad6698b52bdc121ad
Bruno Felix <brunofelix.dev@gmail.com>
2026-07-11 22:43:01 -0300

    🎨 Adding system dark theme support to AppTheme

--- shared/src/commonMain/kotlin/dev/brunofelix/presentation/ui/theme/Theme.kt
+++ shared/src/commonMain/kotlin/dev/brunofelix/presentation/ui/theme/Theme.kt
@@ -1,14 +1,16 @@
 package dev.brunofelix.presentation.ui.theme
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 
 @Composable
 fun AppTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
     MaterialTheme(
-        colorScheme = DarkColorPalette,
+        colorScheme = if (darkTheme) DarkColorPalette else DarkColorPalette,
         typography = appTypography(),
         shapes = AppShapes,
         content = content
