f714926123580c0fe4db44f2a9dd0dc5c54db235
Bruno Felix <brunofelix.dev@gmail.com>
2026-07-12 14:34:36 -0300

    💄 Adding MovieDetailTopBar component

---
+++ shared/src/commonMain/kotlin/dev/brunofelix/presentation/ui/components/MovieDetailTopBar.kt
@@ -0,0 +1,60 @@
+package dev.brunofelix.presentation.ui.components
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import compose.icons.FontAwesomeIcons
+import compose.icons.fontawesomeicons.Solid
+import compose.icons.fontawesomeicons.solid.ChevronLeft
+import dev.brunofelix.presentation.ui.theme.AppTheme
+import movies_kmp.shared.generated.resources.Res
+import movies_kmp.shared.generated.resources.movie_detail_title
+import org.jetbrains.compose.resources.stringResource
+
+@Composable
+fun MovieDetailTopBar(
+    onBack: () -> Unit = {},
+    modifier: Modifier = Modifier
+) {
+    CenterAlignedTopAppBar(
+        modifier = modifier,
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.background
+        ),
+        title = {
+            Text(
+                text = stringResource(Res.string.movie_detail_title),
+                style = MaterialTheme.typography.titleMedium,
+                color = MaterialTheme.colorScheme.primary
+            )
+        },
+        navigationIcon = {
+            IconButton(
+                onClick = onBack
+            ) {
+                Icon(
+                    imageVector = FontAwesomeIcons.Solid.ChevronLeft,
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.primary,
+                    modifier = Modifier.size(22.dp)
+                )
+            }
+        }
+    )
+}
+
+@Preview
+@Composable
+private fun Preview() {
+    AppTheme {
+        MovieDetailTopBar()
+    }
+}
