1b98b730cf014b35f6398f10b4721e76b9e76c94
Bruno Felix <brunofelix.dev@gmail.com>
2026-07-12 12:49:24 -0300

    💄 Adding MovieDetailState for movie details screen

---
+++ shared/src/commonMain/kotlin/dev/brunofelix/presentation/state/MovieDetailState.kt
@@ -0,0 +1,12 @@
+package dev.brunofelix.presentation.state
+
+import androidx.compose.runtime.Immutable
+import dev.brunofelix.domain.model.Movie
+import dev.brunofelix.presentation.util.UiState
+
+@Immutable
+data class MovieDetailState(
+    val state: UiState<Movie> = UiState.Loading,
+    val onBack: () -> Unit = {},
+    val onWatchTrailer: () -> Unit = {}
+)
