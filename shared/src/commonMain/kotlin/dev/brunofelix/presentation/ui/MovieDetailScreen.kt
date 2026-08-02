8e06a961a1810d7102506e13387ae0f705050366
Bruno Felix <brunofelix.dev@gmail.com>
2026-08-02 17:36:24 -0300

    💄 Implementing UI states and movie poster in MovieDetailScreen

--- shared/src/commonMain/kotlin/dev/brunofelix/presentation/ui/MovieDetailScreen.kt
+++ shared/src/commonMain/kotlin/dev/brunofelix/presentation/ui/MovieDetailScreen.kt
@@ -1,16 +1,29 @@
 package dev.brunofelix.presentation.ui
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import co.touchlab.kermit.Logger
+import coil3.compose.AsyncImage
+import dev.brunofelix.domain.model.Movie
 import dev.brunofelix.presentation.state.MovieDetailState
+import dev.brunofelix.presentation.ui.components.ErrorState
+import dev.brunofelix.presentation.ui.components.LoadingState
 import dev.brunofelix.presentation.ui.components.MovieDetailTopBar
 import dev.brunofelix.presentation.ui.theme.AppTheme
 import dev.brunofelix.presentation.util.UiState
+import dev.brunofelix.presentation.util.toReadableMessage
 
 @Composable
 fun MovieDetailRoute(
@@ -40,15 +53,51 @@ private fun MovieDetailScreen(
             MovieDetailTopBar(state.onBack)
         }
     ) { paddingValues ->
-        MovieDetailContent()
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
+            val stateModifier = Modifier.fillMaxSize().padding(paddingValues)
+            when (val uiState = state.uiState) {
+                is UiState.Loading -> LoadingState(stateModifier)
+                is UiState.Success -> {
+                    Surface(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(16.dp)
+                            .weight(1F),
+                        shape = MaterialTheme.shapes.large
+                    ) {
+                        AsyncImage(
+                            model = uiState.data.posterPath,
+                            contentDescription = null,
+                            contentScale = ContentScale.Crop,
+                            modifier = Modifier.clip(MaterialTheme.shapes.medium)
+                        )
+                    }
+                }
+                is UiState.Error -> {
+                    ErrorState(
+                        message = uiState.exception.toReadableMessage(),
+                        modifier = stateModifier
+                    )
+                }
+            }
+        }
     }
 }
 
+@Preview(showBackground = true)
 @Composable
-private fun MovieDetailContent(
-    modifier: Modifier = Modifier
-) {
-
+private fun LoadingPreview() {
+    AppTheme {
+        MovieDetailScreen(
+            state = MovieDetailState(
+                uiState = UiState.Loading
+            )
+        )
+    }
 }
 
 @Preview(showBackground = true)
@@ -56,7 +105,21 @@ private fun MovieDetailContent(
 private fun SuccessPreview() {
     AppTheme {
         MovieDetailScreen(
-            state = MovieDetailState()
+            state = MovieDetailState(
+                uiState = UiState.Success(Movie())
+            )
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ErrorPreview() {
+    AppTheme {
+        MovieDetailScreen(
+            state = MovieDetailState(
+                uiState = UiState.Error(Exception("Error"))
+            )
         )
     }
 }
