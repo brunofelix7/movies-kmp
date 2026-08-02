f082e24d251d0c544b20f1665de5f79eedea38b4
Bruno Felix <brunofelix.dev@gmail.com>
2026-08-02 17:47:56 -0300

    💄 Adding movie title and info labels to MovieDetailScreen

--- shared/src/commonMain/kotlin/dev/brunofelix/presentation/ui/MovieDetailScreen.kt
+++ shared/src/commonMain/kotlin/dev/brunofelix/presentation/ui/MovieDetailScreen.kt
@@ -1,26 +1,42 @@
 package dev.brunofelix.presentation.ui
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import co.touchlab.kermit.Logger
 import coil3.compose.AsyncImage
+import compose.icons.FontAwesomeIcons
+import compose.icons.fontawesomeicons.Solid
+import compose.icons.fontawesomeicons.solid.Calendar
+import compose.icons.fontawesomeicons.solid.Clock
+import compose.icons.fontawesomeicons.solid.Star
 import dev.brunofelix.domain.model.Movie
 import dev.brunofelix.presentation.state.MovieDetailState
 import dev.brunofelix.presentation.ui.components.ErrorState
 import dev.brunofelix.presentation.ui.components.LoadingState
 import dev.brunofelix.presentation.ui.components.MovieDetailTopBar
+import dev.brunofelix.presentation.ui.components.MovieGenreChip
+import dev.brunofelix.presentation.ui.components.MovieInfoLabel
 import dev.brunofelix.presentation.ui.theme.AppTheme
 import dev.brunofelix.presentation.util.UiState
 import dev.brunofelix.presentation.util.toReadableMessage
@@ -75,6 +91,52 @@ private fun MovieDetailScreen(
                             contentScale = ContentScale.Crop,
                             modifier = Modifier.clip(MaterialTheme.shapes.medium)
                         )
+                    }
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .weight(2f)
+                            .padding(top = 20.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                    ) {
+                        Text(
+                            text = uiState.data.title,
+                            textAlign = TextAlign.Center,
+                            style = MaterialTheme.typography.titleLarge,
+                            fontWeight = FontWeight.Bold,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(horizontal = 16.dp)
+                        )
+                        Spacer(modifier = Modifier.height(8.dp))
+                        Row(
+                            modifier = Modifier,
+                            verticalAlignment = Alignment.CenterVertically,
+                        ) {
+                            MovieInfoLabel(
+                                icon = FontAwesomeIcons.Solid.Star,
+                                text = "7.5"
+                            )
+                            Spacer(modifier = Modifier.width(16.dp))
+                            MovieInfoLabel(
+                                icon = FontAwesomeIcons.Solid.Clock,
+                                text = "2h 36 min"
+                            )
+                            Spacer(modifier = Modifier.width(16.dp))
+                            MovieInfoLabel(
+                                icon = FontAwesomeIcons.Solid.Calendar,
+                                text = "2022"
+                            )
+                        }
+                        Spacer(modifier = Modifier.height(8.dp))
+                        Row(
+                            modifier = Modifier,
+                            verticalAlignment = Alignment.CenterVertically,
+                        ) {
+                            MovieGenreChip(
+                                genre = "Action"
+                            )
+                        }
                     }
                 }
                 is UiState.Error -> {
